0f759fed85f55b5bb8db6ebb80bae40dc323fb67
Yun-Jehyeok <54862291+Yun-Jehyeok@users.noreply.github.com>
2024-05-14 18:15:06 +0900

    Use Queries for Login

--- client/src/apis/userApi.ts
+++ client/src/apis/userApi.ts
@@ -1,5 +1,9 @@
-import { signupIFC } from '@/interface/userIFC';
+import { signinIFC, signupIFC } from '@/interface/userIFC';
 import { Apis } from '@/utils/api';
+
+export const signinApi = async (user: signinIFC) => {
+    return await Apis.post('/user/login', user);
+};
 
 export const signupApi = async (user: signupIFC) => {
     return await Apis.post('/user/register', user);
