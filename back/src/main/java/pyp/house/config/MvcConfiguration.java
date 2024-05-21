5fc2a64a40e93f70bea0f231f52f293f61e42890
hoondal6970 <davidtwins6970+1@gmail.com>
2024-05-21 22:44:25 +0900

    Changes

---
+++ back/src/main/java/pyp/house/config/MvcConfiguration.java
@@ -0,0 +1,19 @@
+package pyp.house.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableScheduling
+@EnableTransactionManagement
+public class MvcConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry){
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "PUT", "POST", "DELETE");
+    }
+}
